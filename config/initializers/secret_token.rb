cd829eb785f1df69b43c3fdf6ffed7c9df57cec3
Linda Nguyen <linda@cardspring.com>
2013-08-13 15:41:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fieldwire::Application.config.secret_key_base = '91e36ccc0950b9d86e96119e25da4c143a0a270b6afc1823e1cba11c78f8d766e782b29018313d741305ef33e286b3cea0a81710459c9f4f8f07f1fe2979c8e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fieldwire::Application.config.session_store :cookie_store, key: '_fieldwire_session'
