52bb558418812016ef617c48ab6ef7231c75b1bf
Linda Nguyen <skoryky@gmail.com>
2013-08-13 23:54:01 -0700

    models

--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -3,6 +3,7 @@ class Floorplan < ActiveRecord::Base
   MAX_DISPLAY_NAME_LENGTH = 100
 
   belongs_to :project
+  has_many :blueprints
 
   validates :display_name, :length => {:maximum => MAX_DISPLAY_NAME_LENGTH}, :presence => true
   validates :project, :presence => true
